c50be428b67212dc3f23fa9a68911a2be166f550
ShumkovD <94443037+ShumkovD@users.noreply.github.com>
2022-09-01 09:36:50 +0900

    コメントの追加「PathPoint.cpp」

--- Source/Unreal_VRProject/Private/PathPoint.cpp
+++ Source/Unreal_VRProject/Private/PathPoint.cpp
@@ -3,22 +3,15 @@
 
 #include "PathPoint.h"
 
+//コンストラクタ
 PathPoint::PathPoint(FVector position, int segment): position_(position), segment_(segment)
 {
 }
-
+//デストラクタ
 PathPoint::~PathPoint()
 {
-
-}PathPoint::PathPoint()
-{
-
 }
-
-FVector PathPoint::GetPosition() const
-{
-	return position_;
-}
+//距離の関数
 double PathPoint::GetDistanceToCoordinate(FVector coor)
 {
 	return (coor.X - position_.X) * (coor.X - position_.X) + (coor.Y - position_.Y) * (coor.Y - position_.Y) + (coor.Z - position_.Z) * (coor.Z - position_.Z);
