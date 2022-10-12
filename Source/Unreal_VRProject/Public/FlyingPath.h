0e080ca539e1e39035cbda8b9942ddbbbd761e5d
シュムコヴデニス <shumkovdd@gmail.com>
2022-10-12 20:49:29 +0900

    エフェクトの追加

--- Source/Unreal_VRProject/Public/FlyingPath.h
+++ Source/Unreal_VRProject/Public/FlyingPath.h
@@ -9,68 +9,110 @@
 #include "FlyingPath.generated.h"
 
 
+UCLASS()
 
-
-UCLASS(Blueprintable)
 class UNREAL_VRPROJECT_API AFlyingPath : public AActor
 {
+
 	GENERATED_BODY()
 	
 
 public:	
-//コンストラクタ
 	AFlyingPath();
-	//セグメント
-	TArray<PathSegment> segments;
-	//チェックポイントの配列
-	UPROPERTY(EditAnywhere)
+
+		TArray<PathSegment> segments;
+
+UPROPERTY(EditAnywhere, Category = "チェックポイントの配列")
+
 		TArray<AActor*> pathCheckpoints;
-	//セグメントでの最低距離を持っているポイントを探す範囲「セグメント」
-	UPROPERTY(EditAnywhere)
-	int searchDepth;
-	UPROPERTY(EditAnywhere)
+
+UPROPERTY(EditAnywhere, Category = "探索の深さ、いくつセグメントで最短距離を同時に探しているか")
+
+		int searchDepth;
+
+UPROPERTY(EditAnywhere, Category = "セグメントにある点の間の距離")
+
 		float distanceBetweenPointsOnSegment;
-	//パスに戻される力
-	UPROPERTY(EditAnywhere)
+
+UPROPERTY(EditAnywhere, Category = "パスに戻させる力・速度")
+
 		double speed;
-	UPROPERTY(EditAnywhere)
+
+UPROPERTY(EditAnywhere, Category = "自動的にパスを進む速度")
+
 		double autoSpeed;
 
-	UPROPERTY(EditAnywhere)
+UPROPERTY(EditAnywhere, Category = "アルゴリズムの全体速度の管理")
+
 		double TimeSpeed;
 
+UPROPERTY(EditAnywhere, Category = "使いたいオブジェクトはStartPosで発生するPawnですか？")
 
-	UPROPERTY(EditAnywhere, Category = "移動するオブジェクト")
 		bool actorIsDefault = true;
 
-	//プレイヤのActor「必ず初期化する！！」
-	UPROPERTY(EditAnywhere, meta = (EditCondition = "!actorIsDefault"), Category = "移動するオブジェクト")
+UPROPERTY(EditAnywhere, meta = (EditCondition = "!actorIsDefault"), Category = "StartPos発生するPawn以外のオブジェクトを設定してください")
+
 		AActor* actorToMove;
 
-	//このフレームでキャラクターに一番近いポイント「数はsearchDepthによって違う」
-	TArray<PathPoint> closestPointsToPlayer;
-	//力を与えるために使う一番キャラクターに近いポイント
-	PathPoint closestPoint;
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "飛ぶコース（デバッグ）")
+UPROPERTY(EditAnywhere, Category = "エフェクト・プレイヤーを中心にして、移動する距離")
+
+		int effectSpawnAndDestroyDistance;
+
+UPROPERTY(EditAnywhere, Category = "エフェクト・発生する頻度")
+		float spawnTime;
+
+		
+UPROPERTY(EditAnywhere, Category = "エフェクト")
+		TSubclassOf<AActor> effect;
+		 
+
+//ブループリントで使う変数「エフェクト」
+UPROPERTY(BlueprintReadOnly, Category = "Variable|Blueprint")
+		int startPoint = 0;
+UPROPERTY(BlueprintReadOnly, Category = "Variable|Blueprint")
+		int startSegment = 0;
+UPROPERTY(BlueprintReadOnly, Category = "Variable|Blueprint")
+		int endPoint = 0;
+UPROPERTY(BlueprintReadOnly, Category = "Variable|Blueprint")
+		int endSegment = 0;
+
+
+//今一番近い点の位置 
 		FVector pointPos;
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "飛ぶコース（デバッグ）")
+//次の点の位置
 		FVector nextPointPos;
+//このフレームでキャラクターに一番近いポイント「数はsearchDepthによって違う」
+		TArray<PathPoint> closestPointsToPlayer;
+//力を与えるために使う一番キャラクターに近いポイント
+		PathPoint closestPoint;
+//今飛んでいるセグメント
+		int currentSegment_;
 
-
-
-	int currentSegment_;
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
+
 	virtual void Tick(float DeltaTime) override;
-	//複数セグメントの中で、セグメント毎に一番近いポイントを探す
+
 	TArray<PathPoint> GetClosestPointsOnAreaX(int currentSegmentOnThePath, int x, TArray<PathSegment> seg, FVector playerPos);
-	//一番近いポイントを探す
+
 	PathPoint FindClosestPoint(TArray<PathPoint> points, int& currentSegment, FVector playerPos);
+
+	void SpawnEffects(int differenceFromStartPosition, TArray<PathSegment> seg, PathPoint curPoint);
+
 	PathPoint NextPoint(TArray<PathSegment> seg, PathPoint curPoint);
 
 	void PathMovement(float fixedTime);
+
+	//エフェクトのブループリントで設定する
+	UFUNCTION(BlueprintCallable)
+		void MoveForwardClass(int effectCurrentSegment, int& outNewSegment, int& outCurPoint, float effectSpeed, AActor* effectToMove, float efectFixedTime);
+
+	PathPoint ClosestPoint(int& curSeg, int searchD, TArray<PathSegment> seg, AActor* actor);
+
+private:
+	float currentTick = 0;
+
 };
